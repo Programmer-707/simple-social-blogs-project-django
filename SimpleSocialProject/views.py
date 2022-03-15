20f96b534b8c6e36f4a7ab9bc0e114568ee30498
Muslimbek Abduvaliev <84398564+Programmer-707@users.noreply.github.com>
2022-03-15 20:34:47 +0100

    Add files via upload

---
+++ SimpleSocialProject/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+class homePage(TemplateView):
+    template_name = 'index.html'
+
+class ThanksPage(TemplateView):
+    template_name = 'thanks.html'
+
+class TestPage(TemplateView):
+    template_name = 'test.html'
